43c168526c63606c931fd8589bdc665590a9b0c9
SmirnovaT <75753895+SmirnovaT@users.noreply.github.com>
2021-08-24 16:48:12 +0300

    переделывала классы

--- class_list.py
+++ class_list.py
@@ -2,31 +2,7 @@ class Todo:
     def __init__(self):
         self.items = []
 
-    def input_todo(self, task, percent):
-        self.task = task
-        self.percent = percent
-        sum = f"{task}: {percent}"
-        return self.items.append(sum)
-
-    def remove(self, delete):
-        self.delete = delete
-        for el in self.items:
-            if delete in el:
-                self.items.remove(el)
-            else:
-                print("Данной задачи в списке нет")
-
-    def get_list(self):
-        return self.items
-
-
-obj = Todo()
-
-while True:
-    user = input("Введите команду: ").lower()
-    if user == "показать":
-        print("Список: ", obj.get_list())
-    elif user == "добавить":
+    def input_todo(self):
         task = input("Добавьте задачу в список: ").lower()
         while True:
             try:
@@ -34,15 +10,40 @@ while True:
                 break
             except ValueError:
                 print("Пожалуйста, введите процент выполнения в цифрах")
-        obj.input_todo(task, percent)
-        print("Список: ", obj.get_list())
-    elif user == "удалить":
+
+        sum = f"{task}: {percent}"
+        return self.items.append(sum)
+
+
+    def remove(self):
         delete = input("Что удалить из списка: ").lower()
-        obj.remove(delete)
-        print("Список: ", obj.get_list())
-    elif user == "стоп":
-        break
-    else:
-        print("Заданной команды нет, повторите ввод")
+        for el in self.items:
+            if delete in el:
+                self.items.remove(el)
+            else:
+                print("Данной задачи в списке нет")
+
+    def get_list(self):
+       print("Список: ", self.items)
 
 
+    def init(self):
+        while True:
+            user = input("Введите команду: ").lower()
+            if user == "показать":
+                self.get_list()
+
+            elif user == "добавить":
+                self.input_todo()
+                self.get_list()
+            elif user == "удалить":
+                self.remove()
+                self.get_list()
+            elif user == "стоп":
+                break
+            else:
+                print("Заданной команды нет, повторите ввод")
+
+
+todo_app = Todo()
+todo_app.init()
